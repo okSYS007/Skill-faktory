414bacb13d33260a099ff0c88cf4dcee1c316175
okSYS <ViacheslavDan803@gmail.com>
2022-01-18 20:17:39 +0200

    9.7

--- main.py
+++ main.py
@@ -1,42 +1,19 @@
-class BinaryTree:
-    def __init__(self, value):
-        self.value = value
-        self.left_child = None
-        self.right_child = None
 
-    def insert_left(self, next_value):
-        if self.left_child is None:
-            self.left_child = BinaryTree(next_value)
-        else:
-            new_child = BinaryTree(next_value)
-            new_child.left_child = self.left_child
-            self.left_child = new_child
-        return self
+def binary_search(array, element, left, right): 
+    if left > right: # если левая граница превысила правую,
+        return False # значит элемент отсутствует
+    
+    middle = (right+left) // 2 # находимо середину
+    if array[middle] == element: # если элемент в середине,
+        return middle # возвращаем этот индекс
+    elif element < array[middle]: # если элемент меньше элемента в середине
+        # рекурсивно ищем в левой половине
+        return binary_search(array, element, left, middle-1)
+    else: # иначе в правой
+        return binary_search(array, element, middle+1, right)
 
-    def insert_right(self, next_value):
-        if self.right_child is None:
-            self.right_child = BinaryTree(next_value)
-        else:
-            new_child = BinaryTree(next_value)
-            new_child.right_child = self.right_child
-            self.right_child = new_child
-        return self
+element = int(input())
+array = [i for i in range(1,100)] # 1,2,3,4,...
 
-A_node = BinaryTree('2').insert_left('7').insert_right('5')
-A_node.left_child = BinaryTree('7').insert_left('2').insert_right('6')
-A_node.left_child.right_child = BinaryTree('6').insert_left('5').insert_right('11')
-A_node.right_child = BinaryTree('5').insert_right('9')
-A_node.right_child.right_child.right_child = BinaryTree('4')
-
-# создаем корень и его потомки /7|2|5\
-node_root = BinaryTree(2).insert_left(7).insert_right(5)
-# левое поддерево корня /2|7|6\
-node_7 = node_root.left_child.insert_left(2).insert_right(6)
-# правое поддерево предыдущего узла /5|6|11\
-node_6 = node_7.right_child.insert_left(5).insert_right(11)
-# правое поддерево корня /|5|9\
-node_5 = node_root.right_child.insert_right(9)
-# левое поддерево предыдущего узла корня /4|9|\
-node_9 = node_5.right_child.insert_left(4)
-
-print('ss')
+# запускаем алгоритм на левой и правой границе
+print(binary_search(array, element, 0, 99))
