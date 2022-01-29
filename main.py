7d03a8437d16c2055f6da45b64017d5f9b8ed7f2
okSYS <ViacheslavDan803@gmail.com>
2022-01-29 18:28:55 +0200

    asd

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
+#Напишите программу, которая отправляет запрос на генерацию случайных текстов 
+#Выведите первый из сгенерированных текстов.
 
-def binary_search(array, element, left, right): 
-    if left > right: # если левая граница превысила правую,
-        return False # значит элемент отсутствует
-    
-    middle = (right+left) // 2 # находимо середину
-    if array[middle] == element: # если элемент в середине,
-        return middle # возвращаем этот индекс
-    elif element < array[middle]: # если элемент меньше элемента в середине
-        # рекурсивно ищем в левой половине
-        return binary_search(array, element, left, middle-1)
-    else: # иначе в правой
-        return binary_search(array, element, middle+1, right)
+import requests
+import json # импортируем необходимую библиотеку
+ 
+ 
+r = requests.get('https://baconipsum.com/api/?type=meat-and-filler')
+texts = json.loads(r.content) # делаем из полученных байтов python объект для удобной работы
 
-element = int(input())
-array = [i for i in range(1,100)] # 1,2,3,4,...
-
-# запускаем алгоритм на левой и правой границе
-print(binary_search(array, element, 0, 99))
+print(texts[0])
+print('')
+for i in texts:
+    print(i)
